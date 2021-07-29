f7a466f90f06fa24eed37a57ef38dda9e13c0b51
Roshni1998 <roshnimali847@gmail.com>
2021-07-30 00:20:54 +0530

    Added: UC 5 - Display All Employee Payroll Details from JSON Object

--- js/home.js
+++ js/home.js
@@ -4,23 +4,17 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
 // Template literal E56 feature
 const createInnerHtml = () => {
-  const innerHtml = `
-  <tr>
-    <th></th>
-    <th>Name</th>
-    <th>Gender</th>
-    <th>Department</th>
-    <th>Salary</th>
-    <th>Start Date</th>
-    <th>Actions</th>
-    </tr>
+  const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
+                     "<th>Salary</th><th>Start Date</th><th>Actions</th>";
+  let empPayrollData = createEmployeePayrollJSON()[0];
+  const innerHtml = `${headerHtml}
     <tr>
-      <td><img class="profile" alt="" src="../assets/profile-images/Ellipse -1.png"></td>
-      <td>Roshni Mali</td>
-      <td>Female</td>
-      <td><div class='dept-label'>Sales</div><div class='dept-label'>Finance</div></td>
-      <td>4000000</td>
-      <td>9 Oct 2020</td>
+      <td><img class="profile" alt="" src="${empPayrollData._profilePic}"></td>
+      <td>${empPayrollData._name}</td>
+      <td>${empPayrollData._gender}</td>
+      <td>${getDeptHtml(empPayrollData._department)}</td>
+      <td>${empPayrollData._salary}</td>
+      <td>${empPayrollData._startDate}</td>
       <td>
         <img name="1" onclick="remove(this)" src="../assets/icons/delete-black-18dp.svg" alt="delete">
         <img name="1" onclick="update(this)" src="../assets/icons/create-black-18dp.svg" alt="edit"> 
@@ -28,4 +22,43 @@ const createInnerHtml = () => {
     </tr>
   `;
   document.querySelector('#table-display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+  let empPayrollListLocal = [
+    {
+      _name: 'Roshni Mali',
+      _gender: 'female',
+      _department: [
+        'Sales',
+        'Finance'
+      ],
+      _salary: '500000',
+      _startDate: '29 Oct 2019',
+      _note: '',
+      _id: new Date().getTime(),
+      _profilePic: '../assets/profile-images/Ellipse -1.png'
+    },
+    {
+      _name: 'Shashank Keerthi Kumar',
+      _gender: 'male',
+      _department: [
+        'HR'
+      ],
+      _salary: '300000',
+      _startDate: '29 Oct 2019',
+      _note: '',
+      _id: new Date().getTime() + 1,
+      _profilePic: '../assets/profile-images/Ellipse -2.png'
+    }
+  ];
+  return empPayrollListLocal;
+}
+
+const getDeptHtml = (deptList) => {
+  let deptHtml = '';
+  for (const dept of deptList) {
+    deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`
+  }
+  return deptHtml;
 }
